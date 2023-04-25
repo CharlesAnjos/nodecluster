69a72fb792e985daf78f7e9499ba81edbe123f4f
Charles Albert <charles.ccomp@gmail.com>
2023-04-25 11:04:36 -0300

    alterações feitas nos parametros do commando autocannon

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-
+const cluster = require("cluster")
+const cpus = require("os").cpus
+const pid = require("process").pid
 
 function iniciarServidor() {
     const http = require("http")
 
     const server = http.createServer((req,res) => {
         res.end('Bem-vindo ao site!');
+        //console.debug(`Requsição atendida pelo PID ${pid}`);
     })
 
     const PORT=8000;
-    server.listen(PORT, console.log(`Servidor Iniciado em http://localhost:${PORT}`));
+    server.listen(PORT, console.debug(`Servidor PID ${pid} Iniciado em http://localhost:${PORT}`));
 }
+
+function criarProcessosFilhos(){
+    //console.debug(`Processo pai PID ${pid}`);
+    for (let i=0; i < cpus().length; i++){
+        cluster.fork()
+    }
+}
+
+cluster.isMaster ? criarProcessosFilhos() : iniciarServidor()
